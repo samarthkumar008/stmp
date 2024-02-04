5f9e7adbd910eb79abc2903f813d33a1d3232b62
Akki <92620285+Aanonymous19@users.noreply.github.com>
2024-02-04 11:51:15 +0530

    Add files via upload

---
+++ tests/test.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__.'/../src/abstract.php';
+require_once __DIR__.'/../src/stMp.php';
+
+// Empty Check (1)
+//echo \stMp\stMp::_isEmpty('');
+
+// tolowet
+//echo \stMp\stMp::_upper("Hello WORLDS");
+
+// Trim
+//echo \stMp\stMp::_trim("Hello WORLDS");
+
+// String Lengtb
+//echo \stMp\stMp::_getStrLen('Hello');
+
+//create a dot notation string.
+//echo \stMp\stMp::_dotNotation("Hello Dot");
+
+//so on example you try it
